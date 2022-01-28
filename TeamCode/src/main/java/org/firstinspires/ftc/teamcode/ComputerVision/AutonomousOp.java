d7f9448be784addeb3062d51cdf3262100c4bb82
VladTomici14 <vladtomici@gmail.com>
2022-01-28 21:03:54 +0200

    Created the ComputerVision directory.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComputerVision/AutonomousOp.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode.ComputerVision;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+
+@Autonomous(name = "AutoOp")
+public class AutonomousOp {
+
+    private Robot robot;
+
+    private Controller controller1;
+    private Controller controller2;
+
+    private static final String VUFORIA_KEY = " -- YOUR NEW VUFORIA KEY GOES HERE  --- ";
+
+    private void initVuforia() {
+
+    }
+
+    @Override
+    private void init(){
+		robot = new Robot(hardwareMap, telemetry, Executors.newScheduledThreadPool(1));
+    
+        controller1 = new Controller(gamepad1);
+        controller2 = new Controller(gamepad2);
+
+    }
+
+    @Override
+    private void stop(){
+
+    }
+
+    @Override
+    private void loop(){
+
+    }
+
+}
