43f88e3dcbd86aa1f7eef8bd93e00f8a8c386eeb
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2022-02-17 17:29:03 +0200

    Create barcode detection state and test helper states

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomy/BarcodeTestState.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.autonomy;
+
+import androidx.annotation.NonNull;
+import org.firstinspires.ftc.teamcode.ComputerVision.BarcodeDetector;
+import org.firstinspires.ftc.teamcode.Robot.Robot;
+
+public class BarcodeTestState extends State {
+    BarcodeTestState(@NonNull Robot robot, BarcodeDetector.Position detectedPosition) {
+        super(robot);
+        robot.getTelemetry().addData("Detected barcode position", detectedPosition).setRetained(true);
+    }
+
+    @Override
+    public State update() {
+        if (timePassed >= 5) {
+            return new BarcodeDetectionState(robot);
+        }
+
+        return this;
+    }
+}
