271d67316e61dc786acfda8a7c172e716ec99770
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2022-02-17 17:28:44 +0200

    Create autonomy initial state

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomy/InitialState.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.autonomy;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Robot.Robot;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+class InitialState extends State {
+    InitialState(HardwareMap hardwareMap, Telemetry telemetry, ScheduledExecutorService service) {
+        super(new Robot(hardwareMap, telemetry, service));
+        telemetry.addLine("Initialized initial state");
+    }
+
+    @Override
+    public State update() {
+        return new BarcodeDetectionState(robot);
+    }
+}
