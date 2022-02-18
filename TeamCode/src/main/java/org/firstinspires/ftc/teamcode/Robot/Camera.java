68c25027062bfd0df92a009930e2c0647e1539cf
VladTomici14 <vladtomici@gmail.com>
2022-02-18 12:50:10 +0200

    Cafeluta de buna dimineata cu autonomie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Camera.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.function.Continuation;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.ComputerVision.BarcodeDetector;
 import org.firstinspires.ftc.teamcode.autonomy.BarcodeDetectionPipeline;
 import org.openftc.easyopencv.*;
 
@@ -14,7 +15,10 @@ public class Camera {
     private final Telemetry telemetry;
     private final OpenCvCamera camera;
     private final CompletableFuture<?> cameraOpened;
-    BarcodeDetectionPipeline detector = new BarcodeDetectionPipeline(320);
+
+    private static final int WIDTH = 1920, HEIGHT = 1080;
+
+    BarcodeDetectionPipeline detector = new BarcodeDetectionPipeline(WIDTH, HEIGHT);
 
     Camera(HardwareMap map, Telemetry telemetry) {
         this.telemetry = telemetry;
@@ -31,7 +35,7 @@ public class Camera {
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
-                camera.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
+                camera.startStreaming(WIDTH, HEIGHT, OpenCvCameraRotation.UPRIGHT);
                 cameraOpened.complete(null);
             }
 
@@ -42,16 +46,10 @@ public class Camera {
         });
     }
 
-    public void printLocation() {
-        telemetry.addLine(detector.getLocation().toString());
-    }
-
     public Future<Bitmap> getImage() {
         return cameraOpened.thenCompose(_i -> {
             CompletableFuture<Bitmap> image = new CompletableFuture<>();
             camera.getFrameBitmap(Continuation.createTrivial(image::complete));
-
-            // BarcodeDetectionPipeline.Locations location = detector.getLocation();
 
             return image;
         });
