03792b886c39af688d8d58291a5262fc5270fe76
VladTomici14 <vladtomici@gmail.com>
2022-03-16 20:04:56 +0200

    Added StateException file to the states directory.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomies/AutonomyBlue2.java
@@ -0,0 +1,40 @@
+/*
+ * The tasks that need to be done for the RED 2 scenario:
+ *   -> move right
+ *   -> move forward
+ *   -> rotate 180
+ *   -> throw object
+ *   -> rotate 90 degrees counter clock wise
+ *   -> move left
+ *   -> move forward
+ * */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import org.firstinspires.ftc.teamcode.ComputerVision.BarcodeDetector;
+import org.firstinspires.ftc.teamcode.ComputerVision.StartPosition;
+import org.firstinspires.ftc.teamcode.States.State;
+
+@Autonomous(name = "Red 1")
+@Disabled
+public class AutonomyBlue2 extends OpMode {
+    private State state;
+
+    @Override
+    public void init() {
+        state = State.initial(hardwareMap, telemetry, BarcodeDetector.Position.RIGHT, StartPosition.Position.BLUE2);
+    }
+
+    @Override
+    public void stop() {
+        state.setTime(time);
+        state.stop();
+    }
+
+    @Override
+    public void loop() {
+        state.setTime(time);
+        state = state.update();
+    }
+}
